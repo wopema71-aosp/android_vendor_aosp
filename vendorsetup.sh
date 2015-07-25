7281d26069b0c9dee5cd784008e4442538cbb3b4
wopema71-aosp <wopema71@gmail.co>
2015-07-25 18:31:07 +0100

    change repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/sam-aosp/hudson/master/aosp-build-targets | sed -e 's/#.*$//' | grep android-m-preview | awk {'print $1'})
+for combo in $(curl -s https://raw.githubusercontent.com/wopema71-aosp/hudson/master/aosp-build-targets | sed -e 's/#.*$//' | grep android-m-preview | awk {'print $1'})
 do
     add_lunch_combo $combo
 done
